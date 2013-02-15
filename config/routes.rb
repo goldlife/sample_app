c6051db4e28e9b3c48d8a6f9049e316ee7f8d53e
Leif <lm@bluebox.de>
2013-02-15 10:14:30 +0100

    Add Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
+
+
+  get "users/new"
+
   root to: 'static_pages#home'
 
-  match '/help', to: 'static_pages#help'
+  match '/help' => 'static_pages#help', :as => :help
 
   match '/about', to: 'static_pages#about'
 
